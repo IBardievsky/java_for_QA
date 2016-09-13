68997a540ada74a16c5b85daf6bbcfba3c27ed14
Igarenya <igor_bard@mail.ru>
2016-09-13 10:56:17 +0300

    Реализовал расчет расстояния между точками через функцию "public static double distance"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main (String args[]){
+  public static void main(String args[]) {
+    Point point1 = new Point(7, 10);
+    Point point2 = new Point(3, 5);
 
-	System.out.println("Hello, World!!!");
-}
+    System.out.println(distance(point1, point2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p1.b - p1.a, 2) + Math.pow(p2.b - p2.a, 2));
+  }
 }
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by User on 9/12/2016.
+ */
+public class Point {
+  double a;
+  double b;
+
+  public Point(double p1, double p2) {
+    this.a = p1;
+    this.b = p2;
+  }
+
+}
