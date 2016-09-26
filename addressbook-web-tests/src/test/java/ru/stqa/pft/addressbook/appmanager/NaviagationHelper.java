bda92a8a64002a0c07c82507e4426534f932072d
IBardievsky <igor_bard@mail.ru>
2016-09-26 18:29:43 +0300

    Сделана возможность запуска тестов в 1 из 3 браузеров.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NaviagationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NaviagationHelper.java
@@ -1,15 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by User on 9/22/2016.
  */
 public class NaviagationHelper extends HelperBase {
- private FirefoxDriver wd;
 
-  public NaviagationHelper(FirefoxDriver wd) {
+
+  public NaviagationHelper(WebDriver wd) {
     super(wd);
   }
 
