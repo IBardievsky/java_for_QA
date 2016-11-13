3ed74af2f0ad5c5285e2f33051f4113eed81d5ca
IBardievsky <igor_bard@mail.ru>
2016-11-13 21:15:23 +0200

    Сделан тест по созданию новой задачи в Mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,30 +1,36 @@
 package ru.stqa.pft.mantis.tests;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
+import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.Set;
 
-import static ru.stqa.pft.mantis.tests.TestBase.app;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Home PC on 12.11.2016.
  */
-public class SoapTests {
+public class SoapTests extends TestBase {
 
   @Test
   public void testGenProjects() throws MalformedURLException, ServiceException, RemoteException {
-    MantisConnectPortType mc = new MantisConnectLocator()
-            .getMantisConnectPort(new URL("http://localhost/mantisbt-1.3.3/api/soap/mantisconnect.php"));
-    ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"));
-    System.out.println(projects.length);
-    for (ProjectData project: projects) {
+    Set<Project> projects = app.soap().getProjects();
+    System.out.println(projects.size());
+    for (Project project: projects) {
       System.out.println(project.getName());
     }
+  }
+
+  @Test
+  public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    Set<Project> projects = app.soap().getProjects();
+    Issue issue = new Issue()
+            .withSummary("Test issue").withDescription("Some description").withProject(projects.iterator().next());
+    Issue created = app.soap().addIssue(issue);
+    assertEquals(issue.getSummary(), created.getSummary());
   }
 }
